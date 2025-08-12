11330d14ff36e1b656b9d3cab44609f872f1dceb
onebadduc <conjsp@gmail.com>
2025-08-11 17:01:14 -0700

    finished

--- function.py
+++ function.py
@@ -1,8 +1,71 @@
-def bank():
-    print("bank")
+def payments():
+    print("Choose which type of payments to calculate")
+    print("1. Set Payments")
+    print("2. Payment Amount")
+    i = int(input())
+    if i == 1:
+        amount_of_payments = float(input("Total payment installments: "))
+        total_cost = float(input("Total to be paid: "))
+        cost_per_payment = total_cost / amount_of_payments
+        print("***PAYMENT REPORT***")
+        print(f"Total Payments: {amount_of_payments:.0f}")
+        print(f"Cost Per Payment: {cost_per_payment:.2f}")
+        print("***END REPORT***")
+    elif i == 2:
+        total_cost = float(input("Total to be paid: "))
+        cost_per_payment = float(input("Amount paid per month: "))
+        amount_of_payments = total_cost / cost_per_payment
+        print("***PAYMENT REPORT***")
+        print(f"Total Payments: {amount_of_payments:.0f}")
+        print(f"Cost Per Payment: {cost_per_payment:.2f}")
+        print("***END REPORT***")
+    else:
+        print("Error returning to main menu")
+        print("Please choose a number listed")
+
 
 def calculate_income():
-    print("income")
+    fed_tax = 0.12
+    pay_per_hour = float(input("Enter pay per hour: "))
+    hour_worked = float(input("Enter hours worked this pay period: "))
+    gross_pay = pay_per_hour * hour_worked
+    take_home = gross_pay - (gross_pay * fed_tax)
+    print("***PAY REPORT***")
+    print(f"Pay before tax is: {gross_pay}")
+    print(f"Pay after federal tax is: {take_home}")
+    print("***END REPORT***")
+
 
 def budget():
-    print("budget")
+    period = input("Enter budget period in format(m/d-m/d): ")
+    pay_this_period = float(input("Enter amount to budget: "))
+    money_not_budget = pay_this_period
+    bills = float(input("Enter total amount in bills due: "))
+    money_not_budget -= bills
+    print(f"Money left to budget: ${money_not_budget}")
+    savings = float(input("Enter amount to put into savings: "))
+    money_not_budget -= savings
+    print(f"Money left to budget: ${money_not_budget}")
+    food_cost = float(input("Enter estimate of food cost this period: "))
+    money_not_budget -= food_cost
+    print(f"Money left to budget: ${money_not_budget}")
+    other = float(input("Enter amount you plan on spending on non-essentials: "))
+    print(f"***BUDGET FOR ${period}")
+    print(f"Total Bills: ${bills}")
+    print(f"Total to Save: ${savings}")
+    print(f"Estimate food cost: ${food_cost}")
+    print(f"Other costs: ${other}")
+    if money_not_budget > 0:
+        print(f"Money left over: ${money_not_budget}")
+    print("***END OF BUDGET***")
+    with open("budget_report", "a") as file:
+        file.write("\n")
+        file.write(f"***BUDGET FOR ${period}\n")
+        file.write(f"Total Bills: ${bills}\n")
+        file.write(f"Total to Save: ${savings}\n")
+        file.write(f"Estimate food cost: ${food_cost}\n")
+        file.write(f"Other costs: ${other}\n")
+        if money_not_budget > 0:
+            file.write(f"Money left over: ${money_not_budget}\n")
+        file.write("***END OF BUDGET***\n")
+
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from function import bank
+from function import payments
 from function import calculate_income
 from function import budget
 def main():
     i = 5
     while i != 4:
         print("Please select a program to run by typing the corresponding number")
-        print("1. Bank")
+        print("1. Payments")
         print("2. Calculate Income")
         print("3. Budgeting")
         print("4. Exit")
         i = int(input("Press Enter after selection: "))
         if i == 1:
-            bank()
+            payments()
         elif i == 2:
             calculate_income()
         elif i == 3:
             budget()
+        if i != 4:
+            print("would you like to make another selection?")
+            r = input("y/n: ")
+            if r == "n":
+                i = 4
+
         
 
 
